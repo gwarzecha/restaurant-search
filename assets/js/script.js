12434e92a3d26d30fd3ab281f23b30524890a9e6
gwarz <gmwarzecha@tutanota.com>
2021-02-06 17:11:41 -0700

    broke down main function into separate pieces, added more button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,25 +22,25 @@ window.onload = function() {
 
 
 
- //api search syntax for pixabay imgs
- var imgApiUrl = "https://pixabay.com/api/?q=plated_food" +
- 
- "&image_type=photo&key=" +
- imgApiKey;
- console.log(imgApiUrl);
+//api search syntax for pixabay imgs
+var imgApiUrl = "https://pixabay.com/api/?q=plated_food" +
+  "&image_type=photo&key=" +
+  imgApiKey;
+console.log(imgApiUrl);
 
- fetch(imgApiUrl) 
+// fetch for imgs from pixabay
+fetch(imgApiUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (response) {
     console.log(response);
 
-    var imgList = [2,3,4,7,8,9,13,14,16,17];
-    
+    var imgList = [2, 3, 4, 7, 8, 9, 13, 14, 16, 17];
+
     // picks random image from imgList on page load/refresh to populate the main page photo 
     var randImg = imgList[Math.floor(Math.random() * imgList.length)];
-    
+
     var fetchedPhotos = response.hits[randImg].webformatURL;
     console.log(fetchedPhotos);
 
@@ -54,13 +54,15 @@ window.onload = function() {
 // function to enter city and subsequent cuisine buttons
 fetchLocation.addEventListener("click", function (event) {
   event.preventDefault();
+
+  $("#restaurantContainer").empty();
+
   //console.log("clicked");
   // city name entered within the search field
   var cityName = document.querySelector("#searchInput").value;
   //console.log(cityName);
 
-  $("#restaurantContainer").html("");
-
+  
   localStorage.setItem("city", cityName);
 
   // fetch call to retrieve city data from zomato 
@@ -80,306 +82,126 @@ fetchLocation.addEventListener("click", function (event) {
     .then(function (response) {
       console.log(response);
       // fetches the city id for the first city in the area (which is typically the one searched for)
-      var fetchedId = response.location_suggestions[0].id;
-      //console.log(fetchedId);
+      var cityId = response.location_suggestions[0].id;
+      //console.log(cityId);
 
 
       // icon food-type buttons
       var buttonM = document.querySelector("#iconBtnM");
       buttonM.addEventListener("click", function (event) {
         event.preventDefault();
-        console.log("clicked");
-
-        // all of the below are fetch calls to retrieve cuisine/restuarant data from zomato 
-        var myHeaders = new Headers();
-        myHeaders.append("user-key", zomatoApiKey);
-        var requestOptions = {
-          method: 'GET',
-          headers: myHeaders
-        };
-
-        fetch("https://developers.zomato.com/api/v2.1/search?entity_id=" +
-          fetchedId +
-          "&entity_type=city&cuisines=73", requestOptions)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (response) {
-            console.log(response);
-
-            var list = response.restaurants;
-            //console.log(list);
-
-            $("#restaurantContainer").html("");
-
-            for (var i = 0; i < 5; i++) {
-              console.log(list[i]);
-              if (list[i].restaurant.featured_image.length > 0) {
-                var restaurantCard = //offset-s3 in class
-                `<div class="col s3 card large card-image"> 
-                  <img class="foodImg" src="${list[i].restaurant.featured_image}"/>
-                  <span class="card-title">${list[i].restaurant.name}</span>
-                  <div class="card-content">
-                    <p class="address">${list[i].restaurant.location.address}</p>
-                    <hr>
-                    <p class="hours">${list[i].restaurant.timings}</p>
-                  </div>
-                </div>`
-                //console.log(restaurantCard);
-                //<p class="rating">${list[i].restaurant.price_range}</p>
-
-                $("#restaurantContainer").append(restaurantCard);
-              }
-            }
-          })
+        cuisineResults(cityId, buttonM.value);
       });
 
       var buttonA = document.querySelector("#iconBtnA");
       buttonA.addEventListener("click", function (event) {
         event.preventDefault();
-        //console.log("clicked");
-
-        // fetch call to retrieve data from zomato 
-        var myHeaders = new Headers();
-        myHeaders.append("user-key", zomatoApiKey);
-        var requestOptions = {
-          method: 'GET',
-          headers: myHeaders
-        };
-
-        fetch("https://developers.zomato.com/api/v2.1/search?entity_id=" +
-          fetchedId +
-          "&entity_type=city&cuisines=1", requestOptions)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (response) {
-            console.log(response);
-
-            var list = response.restaurants;
-            //console.log(list);
-
-            $("#restaurantContainer").html("");
-
-            for (var i = 0; i < 5; i++) {
-              console.log(list[i]);
-              if (list[i].restaurant.featured_image.length > 0) {
-                var restaurantCard = //offset-s3 in class
-                `<div class="col s3 card large card-image"> 
-                  <img class="foodImg" src="${list[i].restaurant.featured_image}"/>
-                  <span class="card-title">${list[i].restaurant.name}</span>
-                  <div class="card-content">
-                    <p class="address">${list[i].restaurant.location.address}</p>
-                    <hr>
-                    <p class="hours">${list[i].restaurant.timings}</p>
-                  </div>
-                </div>`
-                //console.log(restaurantCard);
-                //<p class="rating">${list[i].restaurant.price_range}</p>
-
-                $("#restaurantContainer").append(restaurantCard);
-              }
-            }
-          })
+        cuisineResults(cityId, buttonA.value);
       });
 
       var buttonP = document.querySelector("#iconBtnP");
       buttonP.addEventListener("click", function (event) {
         event.preventDefault();
-        //console.log("clicked");
-
-        // fetch call to retrieve data from zomato 
-        var myHeaders = new Headers();
-        myHeaders.append("user-key", zomatoApiKey);
-        var requestOptions = {
-          method: 'GET',
-          headers: myHeaders
-        };
-
-        fetch("https://developers.zomato.com/api/v2.1/search?entity_id=" +
-          fetchedId +
-          "&entity_type=city&cuisines=55", requestOptions)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (response) {
-            console.log(response);
-
-            var list = response.restaurants;
-            //console.log(list);
-
-            $("#restaurantContainer").html("");
-
-            for (var i = 0; i < 5; i++) {
-              console.log(list[i]);
-              if (list[i].restaurant.featured_image.length > 0) {
-                var restaurantCard = //offset-s3 in class
-                `<div class="col s3 card large card-image"> 
-                  <img class="foodImg" src="${list[i].restaurant.featured_image}"/>
-                  <span class="card-title">${list[i].restaurant.name}</span>
-                  <div class="card-content">
-                    <p class="address">${list[i].restaurant.location.address}</p>
-                    <hr>
-                    <p class="hours">${list[i].restaurant.timings}</p>
-                  </div>
-                </div>`
-                //console.log(restaurantCard);
-                //<p class="rating">${list[i].restaurant.price_range}</p>
-
-                $("#restaurantContainer").append(restaurantCard);
-              }
-            }
-          })
+        cuisineResults(cityId, buttonP.value);
       });
 
       var buttonC = document.querySelector("#iconBtnC");
       buttonC.addEventListener("click", function (event) {
         event.preventDefault();
-        //console.log("clicked");
-
-        // fetch call to retrieve data from zomato 
-        var myHeaders = new Headers();
-        myHeaders.append("user-key", zomatoApiKey);
-        var requestOptions = {
-          method: 'GET',
-          headers: myHeaders
-        };
-
-        fetch("https://developers.zomato.com/api/v2.1/search?entity_id=" +
-          fetchedId +
-          "&entity_type=city&cuisines=161", requestOptions)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (response) {
-            console.log(response);
-
-            var list = response.restaurants;
-            //console.log(list);
-
-            $("#restaurantContainer").html("");
-
-            for (var i = 0; i < 5; i++) {
-              console.log(list[i]);
-              if (list[i].restaurant.featured_image.length > 0) {
-                var restaurantCard = //offset-s3 in class
-                `<div class="col s3 card large card-image"> 
-                  <img class="foodImg" src="${list[i].restaurant.featured_image}"/>
-                  <span class="card-title">${list[i].restaurant.name}</span>
-                  <div class="card-content">
-                    <p class="address">${list[i].restaurant.location.address}</p>
-                    <hr>
-                    <p class="hours">${list[i].restaurant.timings}</p>
-                  </div>
-                </div>`
-                //console.log(restaurantCard);
-                //<p class="rating">${list[i].restaurant.price_range}</p>
-
-                $("#restaurantContainer").append(restaurantCard);
-              }
-            }
-          })
+        cuisineResults(cityId, buttonC.value);
       });
 
       var buttonH = document.querySelector("#iconBtnH");
       buttonH.addEventListener("click", function (event) {
         event.preventDefault();
-        //console.log("clicked");
-
-        // fetch call to retrieve data from zomato 
-        var myHeaders = new Headers();
-        myHeaders.append("user-key", zomatoApiKey);
-        var requestOptions = {
-          method: 'GET',
-          headers: myHeaders
-        };
-
-        fetch("https://developers.zomato.com/api/v2.1/search?entity_id=" +
-          fetchedId +
-          "&entity_type=city&cuisines=143", requestOptions)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (response) {
-            console.log(response);
-
-            var list = response.restaurants;
-            //console.log(list);
-
-            $("#restaurantContainer").html("");
-
-            for (var i = 0; i < 5; i++) {
-              console.log(list[i]);
-              if (list[i].restaurant.featured_image.length > 0) {
-                var restaurantCard = //offset-s3 in class
-                `<div class="col s3 card large card-image"> 
-                  <img class="foodImg" src="${list[i].restaurant.featured_image}"/>
-                  <span class="card-title">${list[i].restaurant.name}</span>
-                  <div class="card-content">
-                    <p class="address">${list[i].restaurant.location.address}</p>
-                    <hr>
-                    <p class="hours">${list[i].restaurant.timings}</p>
-                  </div>
-                </div>`
-                //console.log(restaurantCard); 
-                //<p class="rating">${list[i].restaurant.price_range}</p>
-
-                $("#restaurantContainer").append(restaurantCard);
-              }
-            }
-          })
+        cuisineResults(cityId, buttonH.value);
       });
 
       var buttonJ = document.querySelector("#iconBtnJ");
       buttonJ.addEventListener("click", function (event) {
         event.preventDefault();
-        //console.log("clicked");
-
-        // fetch call to retrieve data from zomato 
-        var myHeaders = new Headers();
-        myHeaders.append("user-key", zomatoApiKey);
-        var requestOptions = {
-          method: 'GET',
-          headers: myHeaders
-        };
-
-        fetch("https://developers.zomato.com/api/v2.1/search?entity_id=" +
-          fetchedId +
-          "&entity_type=city&cuisines=3", requestOptions)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (response) {
-            console.log(response);
-
-            var list = response.restaurants;
-            //console.log(list);
-
-            $("#restaurantContainer").html("");
-
-            for (var i = 0; i < 5; i++) {
-              console.log(list[i]);
-              if (list[i].restaurant.featured_image.length > 0) {
-                var restaurantCard = //offset-s3 in class
-                  `<div class="col s3 card large card-image"> 
-                    <img class="foodImg" src="${list[i].restaurant.featured_image}"/>
-                    <span class="card-title">${list[i].restaurant.name}</span>
-                    <div class="card-content">
-                      <p class="address">${list[i].restaurant.location.address}</p>
-                      <hr>
-                      <p class="hours">${list[i].restaurant.timings}</p>
-                    </div>
-                  </div>`
-                  //console.log(restaurantCard);
-                  //<p class="rating">${list[i].restaurant.price_range}</p>
-
-                $("#restaurantContainer").append(restaurantCard);
-              }
-            }
-          })
+        cuisineResults(cityId, buttonJ.value);
       });
+
+      var buttonB = document.querySelector("#iconBtnB");
+      buttonB.addEventListener("click", function (event) {
+        event.preventDefault();
+        cuisineResults(cityId, buttonB.value);
+      });
+
+      var buttonS = document.querySelector("#iconBtnS");
+      buttonS.addEventListener("click", function (event) {
+        event.preventDefault();
+        cuisineResults(cityId, buttonS.value);
+      });
+
+      var buttonF = document.querySelector("#iconBtnF");
+      buttonF.addEventListener("click", function (event) {
+        event.preventDefault();
+        cuisineResults(cityId, buttonF.value);
+      });
+
+      var buttonD = document.querySelector("#iconBtnD");
+      buttonD.addEventListener("click", function (event) {
+        event.preventDefault();
+        cuisineResults(cityId, buttonD.value);
+      });
+
     })
+
 });
+
+
+function cuisineResults(cityId, btnValue) {
+
+  console.log("clicked", btnValue);
+
+  $("#restaurantContainer").empty();
+
+  // all of the below are fetch calls to retrieve cuisine/restuarant data from zomato 
+  var myHeaders = new Headers();
+  myHeaders.append("user-key", zomatoApiKey);
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders
+  };
+
+  fetch("https://developers.zomato.com/api/v2.1/search?entity_id=" +
+    cityId +
+    "&entity_type=city&cuisines=" +
+    btnValue
+    , requestOptions)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+
+      var list = response.restaurants;
+      //console.log(list);
+
+      for (var i = 0; i < 5; i++) {
+        console.log(list[i]);
+        var imgUrl = list[i].restaurant.featured_image.length > 0 ? list[i].restaurant.featured_image : "./assets/imgs/image-card-temp.png"
+        //if (list[i].restaurant.featured_image.length > 0) {
+          var restaurantCard = //offset-s3 in class
+            `<div class="col s3 card large card-image"> 
+          <img class="foodImg" src="${imgUrl}"/>
+          <span class="card-title">${list[i].restaurant.name}</span>
+          <div class="card-content">
+            <p class="address">${list[i].restaurant.location.address}</p>
+            <hr>
+            <p class="hours">${list[i].restaurant.timings}</p>
+          </div>
+        </div>`
+          //console.log(restaurantCard);
+          //<p class="rating">${list[i].restaurant.price_range}</p>
+
+          $("#restaurantContainer").append(restaurantCard);
+        //}
+      }
+    })
+};
 
 
 
